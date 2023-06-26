5b64dd00911bb33c402bfb07ccf72cb9560c4550
Felipe-Souza-Marra <felipemarra007@hotmail.com>
2023-06-26 14:15:22 -0300

    feat: adicionado novas rotas e recursos

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
-const express = require('express')
-const mustacheExpress = require('mustache-express')
-const db = require('./src/db')
+const express = require('express');
+const mustacheExpress = require('mustache-express');
+const db = require('./src/db');
+const sessao = require('./src/models/usuario/autenticadorUsuario');
 
-const app = express()
+const app = express();
 
-app.engine('html', mustacheExpress())
-app.set('view engine', 'html')
-app.set('views', __dirname + '/src/views')
+app.use(sessao.criarSessao());
 
-app.use(express.urlencoded({extended: true}))
+app.engine('html', mustacheExpress());
+app.set('view engine', 'html');
+app.set('views', __dirname + '/src/views');
+
+app.use(express.urlencoded({ extended: true }));
 
 // Define as rotas da aplicação (declaradas na pasta /src/routes/)
+app.get('/', function (req, res) { res.render('index.html') })
+app.use('/', require('./src/routes/contaCorrenteRoutes'));
+app.use('/', require('./src/routes/movimentoRoutes'));
 app.use('/', require('./src/routes/pessoaRoutes'));
+app.use('/', require('./src/routes/usuarioRoutes'));
 
 db.sync(() => console.log(`Banco de dados conectado`));
 
 const app_port = 8000
 app.listen(app_port, function () {
-    console.log('app rodando na porta ' + app_port)
+  console.log('app rodando na porta ' + app_port)
 })
